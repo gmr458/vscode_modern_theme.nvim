36351f0bcfb24c111027bd66b93dfc9eb635907c
German David <germanmarinrolong@gmail.com>
2024-08-17 13:09:10 -0500

    let neovimchoose between dark or light depending on the terminal

--- lua/vscode_modern/init.lua
+++ lua/vscode_modern/init.lua
@@ -9,6 +9,7 @@ local M = {}
 --- @field custom_dark_background? string
 --- @field custom_light_background? string
 --- @field custom_statusline_dark_background? string
+--- @field cache_path? string
 M.config = {
     cursorline = false,
     transparent_background = false,
@@ -18,67 +19,57 @@ M.config = {
     custom_dark_background = nil,
     custom_light_background = nil,
     custom_statusline_dark_background = nil,
+    cache_path = vim.fn.stdpath 'cache' .. '/vscode_modern_theme/cache',
 }
-
-local path_compiled_files =
-    string.format('%s%s', vim.fn.stdpath 'cache', '/vscode_modern')
-local path_sep = jit and (jit.os == 'Windows' and '\\' or '/')
-    or package.config:sub(1, 1)
 
 --- @overload fun(config?: Config)
 function M.setup(config)
     M.config = vim.tbl_deep_extend('force', M.config, config or {})
 end
 
+local function compile_if_not_exist()
+    if vim.fn.filereadable(M.config.cache_path) == 0 then
+        local palette = require 'vscode_modern.palette'
+
+        local theme_dark =
+            require('vscode_modern.themes').dark(palette, M.config)
+        local theme_light =
+            require('vscode_modern.themes').light(palette, M.config)
+
+        M.compile(M.config, theme_dark, theme_light)
+    end
+end
+
 function M.load()
-    local dark_compiled =
-        string.format('%s%s%s', path_compiled_files, path_sep, 'dark')
-    if not vim.loop.fs_stat(dark_compiled) then
-        local palette = require 'vscode_modern.palette'
-        local theme = require('vscode_modern.themes').dark(palette, M.config)
-        M.compile(M.config, theme, 'dark')
-    end
+    compile_if_not_exist()
 
-    local light_compiled =
-        string.format('%s%s%s', path_compiled_files, path_sep, 'light')
-    if not vim.loop.fs_stat(light_compiled) then
-        local palette = require 'vscode_modern.palette'
-        local theme = require('vscode_modern.themes').light(palette, M.config)
-        M.compile(M.config, theme, 'light')
+    local f = loadfile(M.config.cache_path)
+    if f ~= nil then
+        f()
+    else
+        vim.notify(
+            '[vscode_modern_theme.nvim] error trying to load cache file',
+            vim.log.levels.ERROR
+        )
     end
-
-    local path_compiled =
-        string.format('%s%s%s', path_compiled_files, path_sep, vim.o.background)
-    local f = assert(
-        loadfile(path_compiled),
-        '[vscode_modern.nvim] could not load cache'
-    )
-    f()
 end
 
 --- @param config Config
---- @param theme ThemeDark | ThemeLight
---- @param filename_compiled_colorscheme string
-function M.compile(config, theme, filename_compiled_colorscheme)
+--- @param theme_dark ThemeDark
+--- @param theme_light ThemeLight
+function M.compile(config, theme_dark, theme_light)
     local lines = {
-        string.format(
-            [[
-return string.dump(function()
-vim.o.termguicolors=true
-if vim.g.colors_name then vim.cmd "hi clear" end
+        string.format [[return string.dump(function()
+vim.cmd.highlight('clear')
 vim.g.colors_name="vscode_modern"
-vim.o.background="%s"
-local h=vim.api.nvim_set_hl]],
-            filename_compiled_colorscheme
-        ),
+local h=vim.api.nvim_set_hl
+vim.opt.termguicolors=true]],
     }
 
-    if path_sep == '\\' then
-        path_compiled_files = path_compiled_files:gsub('/', '\\')
-    end
-
-    local hgs = require('vscode_modern.highlight_groups').get(config, theme)
-    for group, color in pairs(hgs) do
+    table.insert(lines, 'if vim.o.background == \'dark\' then')
+    local hgs_dark =
+        require('vscode_modern.highlight_groups').get(config, theme_dark)
+    for group, color in pairs(hgs_dark) do
         table.insert(
             lines,
             string.format(
@@ -88,55 +79,69 @@ local h=vim.api.nvim_set_hl]],
             )
         )
     end
+
+    table.insert(lines, 'else')
+
+    local hgs_light =
+        require('vscode_modern.highlight_groups').get(config, theme_light)
+    for group, color in pairs(hgs_light) do
+        table.insert(
+            lines,
+            string.format(
+                [[h(0,"%s",%s)]],
+                group,
+                vim.inspect(color, { newline = '', indent = '' })
+            )
+        )
+    end
+    table.insert(lines, 'end')
+
     table.insert(lines, 'end,true)')
 
-    if vim.fn.isdirectory(path_compiled_files) == 0 then
-        vim.fn.mkdir(path_compiled_files, 'p')
+    local cache_dir = vim.fn.stdpath 'cache' .. '/vscode_modern_theme/'
+
+    if vim.fn.isdirectory(cache_dir) == 0 then
+        vim.fn.mkdir(cache_dir, 'p')
     end
 
     local f = loadstring(table.concat(lines, '\n'))
     if not f then
-        local err_path = (path_sep == '/' and '/tmp' or os.getenv 'TMP')
-            .. '/vscode_modern_error.lua'
-        print(
-            string.format(
-                '[vscode_modern.nvim] error, open %s for debugging',
-                err_path
-            )
+        local path_debug_file = vim.fn.stdpath 'state'
+            .. '/vscode_modern_theme-debug.lua'
+
+        local msg = string.format(
+            '[vscode_modern_theme.nvim] error, open %s for debugging',
+            path_debug_file
         )
-        local err = io.open(err_path, 'wb')
-        if err then
-            err:write(table.concat(lines, '\n'))
-            err:close()
+        vim.notify(msg, vim.log.levels.ERROR)
+
+        local debug_file = io.open(path_debug_file, 'wb')
+        if debug_file then
+            debug_file:write(table.concat(lines, '\n'))
+            debug_file:close()
         end
         return
     end
 
-    local err_msg = string.format(
-        '[vscode_modern.nvim] permission denied while writing compiled file to %s%s%s',
-        path_compiled_files,
-        path_sep,
-        filename_compiled_colorscheme
-    )
-    local file = assert(
-        io.open(
-            path_compiled_files .. path_sep .. filename_compiled_colorscheme,
-            'wb'
-        ),
-        err_msg
-    )
-    file:write(f())
-    file:close()
+    local file = io.open(cache_dir .. '/cache', 'wb')
+    if file then
+        file:write(f())
+        file:close()
+    else
+        vim.notify(
+            '[vscode_modern_theme.nvim] error trying to open cache file',
+            vim.log.levels.ERROR
+        )
+    end
 end
 
 vim.api.nvim_create_user_command('VSCodeModernCompile', function()
     local palette = require 'vscode_modern.palette'
 
-    local dark_theme = require('vscode_modern.themes').dark(palette, M.config)
-    M.compile(M.config, dark_theme, 'dark')
+    local theme_dark = require('vscode_modern.themes').dark(palette, M.config)
+    local theme_light = require('vscode_modern.themes').light(palette, M.config)
 
-    local light_theme = require('vscode_modern.themes').light(palette, M.config)
-    M.compile(M.config, light_theme, 'light')
+    M.compile(M.config, theme_dark, theme_light)
 
     vim.notify '[vscode_modern.nvim] colorscheme compiled'
     vim.cmd.colorscheme 'vscode_modern'
